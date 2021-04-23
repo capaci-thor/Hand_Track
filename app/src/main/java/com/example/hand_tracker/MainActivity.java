03ed365b346dbb8c0e33f5f5a93c1cf10e74c1c0
Eduardo Trejo <rotjeot@gmail.com>
2021-04-22 22:05:11 -0500

    created button for video activity

--- app/src/main/java/com/example/hand_tracker/MainActivity.java
+++ app/src/main/java/com/example/hand_tracker/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.hand_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.opencv.android.OpenCVLoader;
+import  org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        OpenCVLoader.initDebug();
+
+        Button btnVideo = (Button) findViewById(R.id.btnVideo);
+        btnVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Buton!!!");
+                Intent intent = new Intent(v.getContext(), Video.class);
+                startActivity(intent);
+
+            }
+        });
     }
+
 }
